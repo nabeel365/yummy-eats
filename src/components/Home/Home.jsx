e1592a476891c93688db3eb2bcbe9e4c3d421e57
nabeel365 <choudhurinabeel@gmail.com>
2023-05-01 23:39:31 +0530

    navbar added

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import { Outlet } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+
+            <Outlet></Outlet>          
+
+
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default Home;
