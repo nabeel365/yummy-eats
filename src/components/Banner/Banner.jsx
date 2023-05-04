3e3765fe972aba6777e842719a50e3d47656385c
nabeel365 <choudhurinabeel@gmail.com>
2023-05-04 18:34:42 +0530

    banner updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,15 +6,27 @@ import './Banner.css'
 const Banner = () => {
    
     return (
-        <div className='w-auto bg-img'>
+        <div className='w-auto'>
 
-            <div className=''>
+            {/* <div className=''>
 
                 <h1 className='text-orange-400 sm:text-5xl text-2xl font-extrabold '>Welcome To Yummy Eats</h1>
                 <br />
-                <p className='sm:text-3xl'>Yummy Eats is a popular food recipe website that features a diverse range of delicious and easy-to-follow recipes for food lovers of all levels. From quick and simple meals to more complex dishes, Yummy Eats has something for everyone.</p>
+                <p className='sm:text-3xl'></p>
 
-            </div>
+            </div> */}
+
+            <div className="hero min-h-screen" style={{ backgroundImage: `url("https://www.maggi.co.uk/sites/default/files/styles/maggi_desktop_image_style/public/NUK1265%20maggi%20Recipes%20banner%201500x700px%20opt2A.jpg?h=4f5b30f1&itok=DcsF1RwA")` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Welcome To Yummy Eats</h1>
+      <p className="mb-5">Yummy Eats is a popular food recipe website that features a diverse range of delicious and easy-to-follow recipes for food lovers of all levels. From quick and simple meals to more complex dishes, Yummy Eats has something for everyone.
+      </p>
+      <button className="btn btn-warning">Get Started</button>
+    </div>
+  </div>
+</div>
 
             <br />
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,10 +6,13 @@ import ActiveLink from '../../ActiveLink/ActiveLink';
 
 const Navbar = () => {
 
-    const {logOut} = useContext(AuthContext);
+    const {logOut, user, setUser} = useContext(AuthContext);
     
     const handleLogOut = ()=> {
         logOut();
+        setUser({})
+        .then(result => {})
+        .catch(error => console.log(error));
     }
 
 
@@ -44,7 +47,7 @@ const Navbar = () => {
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                <img />
                             </div>
                         </label>
                         <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
